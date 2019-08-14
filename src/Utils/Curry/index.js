5bbe76747f650f5bdc72c3cafd782ec83729be61
Mario Brusarosco <mariobrusarosco@hotmail.com>
2019-08-14 01:02:29 -0300

    Curry little improvement

--- src/Utils/Curry/index.js
+++ src/Utils/Curry/index.js
@@ -1,14 +1,16 @@
 const Curry = function() {
   const fn = arguments[0]
   const expectedArgs = fn.length
-  let args = []
+  let alreadyGivenArgs = []
 
   return function wrapperFunction() {
-    args = [...args, ...arguments]
+    alreadyGivenArgs = [...alreadyGivenArgs, ...arguments]
 
     if(args.length >= expectedArgs) {
-      const finalArguments = args.slice(0, expectedArgs)
-      args = []
+      // Ignoring extra arguments that may be passed by the users on the last time they call the Curried function
+      const finalArguments = alreadyGivenArgs.slice(0, expectedArgs)
+      // Clean all the previous given arguments
+      alreadyGivenArgs = []
 
       return fn(...finalArguments)
     }
